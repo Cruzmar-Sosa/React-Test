3f02527135089610b26bd0eb1ccf7c71454b8d9f
Cruzmar-Sosa <126338374+Cruzmar-Sosa@users.noreply.github.com>
2024-08-15 01:04:09 -0600

    fix(frontend/utils): fix bug that caused bad birthday date

--- FRONTEND/src/utils/calculateBirthdate.js
+++ FRONTEND/src/utils/calculateBirthdate.js
@@ -1,31 +1,20 @@
-const calculateBirthdate = cedula => {
+const calculateBirthdate = (cedula) => {
   const day = cedula.slice(4, 6);
   const month = cedula.slice(6, 8);
   const shortYear = cedula.slice(8, 10);
+
+  const currentYear = new Date().getFullYear();
+  const fullYear = currentYear % 100;
   
-  //dias
-  const currentDay = new Date().getDate();
-  const birthDay = currentDay - day
-  
-  //mes
-  const currentMonth = new Date().getMonth();
-  const birthMonth = (currentMonth + 1 ) - month
-
-  //aÃ±o
-  let currentYear = new Date().getFullYear();
-  currentYear = currentYear % 100
-  let birthYear = currentYear - shortYear
-
-
-
-  if (birthYear >= 0 && birthMonth >= 0 && birthDay >= 0) {
-    birthYear = 2000 + parseInt(shortYear, 10);
+  // Determinar el siglo
+  let birthYear = parseInt(shortYear, 10);
+  if (birthYear > fullYear) {
+      birthYear += 1900;
   } else {
-    birthYear = 1900 + parseInt(shortYear, 10);
+      birthYear += 2000;
   }
 
-  // Resultado: Fecha de nacimiento completa
+  // Fecha de nacimiento completa
   return `${day}/${month}/${birthYear}`;
 };
-
-export default calculateBirthdate
+export default calculateBirthdate;
